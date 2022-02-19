e8245d60a08239025c67f281196eef1165a8c27f
emilio <emilio.tanowe.maddalena@gmail.com>
2022-02-19 16:24:55 +0100

    add html folder and form page

--- not_just_tires/main.py
+++ not_just_tires/main.py
@@ -2,9 +2,30 @@
 This will become a nice website in the future!
 '''
 
+from crypt import methods
 from flask import Flask
+from flask import render_template, request, redirect
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    my_msg = "We greet the user to our beautiful web page."
+    return render_template("index.html", message=my_msg)
+
+@app.route("/form", methods=["GET", "POST"])
+def form():
+    if request.method == "GET":
+        # the user is requesting the form
+        return render_template("form.html")
+    
+    elif request.method == "POST":
+        # the user has filled the form and the info is available
+        if len(request.form) == 3: 
+            print("You've filled the complete thing!")
+        else:
+            print("Please, complete the whole thing!")
+        return render_template("form.html")
+
+if __name__ == '__main__':
+    app.run()
