6c71361db5ef0c76a1dfab3fea50d3741ce2711a
emilio <emilio.tanowe.maddalena@gmail.com>
2022-03-09 14:31:43 +0100

    feat: added SQLite support
    Now information sent by the user through the app form is stored in a SQLite file.

--- not_just_tires/main.py
+++ not_just_tires/main.py
@@ -4,14 +4,41 @@ This will become a nice website in the future!
 
 from crypt import methods
 from flask import Flask
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+
+import secrets
 
 app = Flask(__name__)
+app.secret_key = secrets.APP_KEY
+
+table_name = 'my_table'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + table_name + '.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Entry(db.Model):
+    
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column("name", db.String(100))
+    age = db.Column("age", db.String(100))
+    desert = db.Column("desert", db.String(100))
+    
+    def __init__(self, name, age, desert):
+        self.name = name
+        self.age = age
+        self.desert = desert
 
 @app.route('/')
 def index():
     my_msg = "We greet the user to our beautiful web page."
     return render_template("index.html", message=my_msg)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template("error.html"), 404
 
 @app.route("/form", methods=["GET", "POST"])
 def form():
@@ -20,12 +47,18 @@ def form():
         return render_template("form.html")
     
     elif request.method == "POST":
-        # the user has filled the form and the info is available
-        if len(request.form) == 3: 
-            print("You've filled the complete thing!")
-        else:
-            print("Please, complete the whole thing!")
+        # the user has submitted something
+        if not all(request.form.values()): print("Please, complete the whole form before submitting it.")
+        form = request.form # that's a dict
+        
+        # adding form data into DB
+        entry = Entry(name=form["name"], age=form["age"], desert=form["desert"])
+        db.session.add(entry)
+        db.session.commit()
+        
+        print('You\'ve successfully submitted your data! 🌝')
         return render_template("form.html")
-
+        
 if __name__ == '__main__':
+    db.create_all()
     app.run()
