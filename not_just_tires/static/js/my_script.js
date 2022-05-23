88d4a7db4bbd7ab2e4be8f93db1e84d2f6e687ad
emilioMaddalena <emilio.tanowe.maddalena@gmail.com>
2022-05-23 10:35:21 +0200

    use JS to create new elements

--- not_just_tires/static/js/my_script.js
+++ not_just_tires/static/js/my_script.js
@@ -1,2 +1,23 @@
-document.write("<h1> HELLO FROM JS <\h1>")
-console.log("it worked!")
+// import bootstrap symbols here?
+
+function printTransactions(trans) {
+    
+    for (let step = 0; step < trans.length; step++) {
+    
+        // Create, fill and post one card per transaction
+        let card = document.createElement('div');
+        fillTransCard(card, trans[step])
+        document.getElementById("main-div").appendChild(card);
+
+    }
+
+}
+
+function fillTransCard(card, trans) {
+
+    const { ID, comprador, data, observacoes, preco_unitario, quantidade, tipo_pneu, tipo_transacao } = trans;
+
+    card.className = 'alert alert-primary';
+    card.innerText = comprador;
+
+}
