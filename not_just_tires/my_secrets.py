2c80c368cbce0571a37ca18c85504823682283a6
emilioMaddalena <emilio.tanowe.maddalena@gmail.com>
2022-06-15 11:16:49 +0200

    necessary update

--- not_just_tires/my_secrets.py
+++
@@ -1,9 +0,0 @@
-DB_ENDPOINT = 'ec2-54-147-33-38.compute-1.amazonaws.com'
-DB_PORT = '5432'
-
-DB_USER = 'rsslshcjzteuux'
-DB_PASSWORD = 'b8501ccf7a78b68f2be98a814de210b75005aa12331bfea5f9a9c8bfaba17788'
-
-DB_NAME = 'd6b6toed2fpo9v'
-
-FLASK_APP_KEY = '3hikccqkcN83qa1ur00cnqkwlfd1'
