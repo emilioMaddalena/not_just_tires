7d82286c4f033466b035bc7c49c3f434f318844e
emilio <emilio.tanowe.maddalena@gmail.com>
2022-08-30 17:47:42 +0200

    add new report cards

--- utils/report.py
+++ utils/report.py
@@ -1,13 +1,15 @@
 def generate_report(data):
     
-    report = {'entradas': compute_entradas(data),
-              'saidas': compute_saidas(data),
+    report = {'valor_vendas': compute_valor_vendas(data),
+              'valor_compras': compute_valor_compras(data),
+              'num_vendas': compute_num_vendas(data),
+              'num_compras': compute_num_compras(data),
               'recusa': compute_recusas(data),
               'balanco_estoque': compute_balanco_estoque(data)}
     
     return report
 
-def compute_entradas(data):
+def compute_valor_vendas(data):
     
     total_global = 0;
     for transac in data['transacoes']:
@@ -16,7 +18,7 @@ def compute_entradas(data):
 
     return total_global
 
-def compute_saidas(data):
+def compute_valor_compras(data):
 
     total_global = 0;
     for transac in data['transacoes']:
@@ -24,6 +26,22 @@ def compute_saidas(data):
         total_global += total_transac
 
     return total_global
+
+def compute_num_vendas(data):
+    
+    num = 0;
+    for transac in data['transacoes']:
+        num = num + 1 if transac['tipo_transacao'] == 'Venda' else num
+
+    return num
+
+def compute_num_compras(data):
+    
+    num = 0;
+    for transac in data['transacoes']:
+        num = num + 1 if transac['tipo_transacao'] == 'Compra' else num
+
+    return num
 
 def compute_recusas(data):
     
