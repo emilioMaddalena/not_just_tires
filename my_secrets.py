0fca832465705e46ff9a97d0ecb83d48657ffca4
emilio <emilio.tanowe.maddalena@gmail.com>
2022-09-13 08:43:23 +0200

    update key

--- my_secrets.py
+++ my_secrets.py
@@ -1,13 +1,6 @@
-DB_ENDPOINT = 'ec2-54-147-33-38.compute-1.amazonaws.com'
-DB_PORT = '5432'
+FLASK_APP_KEY = '3hikccqkcN83qa1ur00cnqkßwlfd1'
 
-DB_USER = 'rsslshcjzteuux'  
-DB_PASSWORD = 'b8501ccf7a78b68f2be98a814de210b75005aa12331bfea5f9a9c8bfaba17788'
-
-DB_NAME = 'd6b6toed2fpo9v'
-
-FLASK_APP_KEY = '3hikccqkcN83qa1ur00cnqkwlfd1'
-
+# remember to replace postgres by postgresql
 DB_FULL_URL = 'postgresql://wbdzisuqxzpttp:3e28a1a02b74e5aeb48c3d5447739b85f6ae6ec2408df2e50c5e0b1986316529@ec2-18-204-142-254.compute-1.amazonaws.com:5432/dfs8kvd95c4acs'
 
 PASSWORD_CLEAR_DB = '98c10ijsj((102j1@j1wMQWiq910lp!ac'
