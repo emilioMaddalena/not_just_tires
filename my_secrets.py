898a2e2059d747d464ddaf5cd37d0ef4956b9adf
emilioMaddalena <emilio.tanowe.maddalena@gmail.com>
2024-12-14 19:11:36 +0100

    Delete things
    - Things that should have been ENV variables

--- my_secrets.py
+++
@@ -1,7 +0,0 @@
-FLASK_APP_KEY = '3hikccqkcN83qa1ur00cnqkßwlfd1'
-
-# remember to replace postgres by postgresql
-#DB_FULL_URL = 'postgresql://wbdzisuqxzpttp:3e28a1a02b74e5aeb48c3d5447739b85f6ae6ec2408df2e50c5e0b1986316529@ec2-18-204-142-254.compute-1.amazonaws.com:5432/dfs8kvd95c4acs'
-DB_FULL_URL = 'postgresql://yonttdpjdqocfb:be59f94f42d25469b0318f5db46e9e8f450fffc16415f9fa3ade5b3cab4f0270@ec2-34-231-63-30.compute-1.amazonaws.com:5432/db036vd6heafej'
-
-PASSWORD_CLEAR_DB = '98c10ijsj((102j1@j1wMQWiq910lp!ac'
